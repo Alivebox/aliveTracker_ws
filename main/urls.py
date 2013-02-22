998ef1450809598a1ce349ef0490dd40db6b30bd
Enrique Valverde <evalverde@alivebox.com>
2013-02-22 11:10:48 -0600

    Web services tests

--- main/urls.py
+++ main/urls.py
@@ -3,7 +3,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from main import views
 
 urlpatterns = patterns('',
-                       url(r'^main/(?P<email>[0-9]+)/$', views.User.as_view()),
+                       url(r'^main/(?P<id>[0-9]+)/$', 'main.views.user_authentication'),
                        )
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- main/views.py
+++ main/views.py
@@ -1,25 +1,20 @@
 from main.models import User
 from main.serializers import UserSerializer
-from django.http import Http404
-from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-import pdb;
+from rest_framework.decorators import api_view
 
 
-class UserAuthentication(APIView):
+@api_view(['GET'])
+def user_authentication(request, id, format=None):
     """
     Retrieve, update or delete a snippet instance.
     """
-    def get_object(self, email):
-        try:
-            pdb.set_trace();
-            return User.objects.get(email=email)
-        except User.DoesNotExist:
-            raise Http404
+    try:
+        user = User.objects.get(id=id)
+    except User.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
-    def get(self, request, email, format=None):
-        pdb.set_trace();
-        user = self.get_object(email)
+    if request.method == 'GET':
         serializer = UserSerializer(user)
         return Response(serializer.data)
