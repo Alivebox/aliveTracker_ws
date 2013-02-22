89dd086f8f138afcb244188f49c448b8b4115120
Enrique Valverde <evalverde@alivebox.com>
2013-02-22 11:11:13 -0600

    Web services tests

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import patterns, url
 from rest_framework.urlpatterns import format_suffix_patterns
-from main import views
 
 urlpatterns = patterns('',
                        url(r'^main/(?P<id>[0-9]+)/$', 'main.views.user_authentication'),
