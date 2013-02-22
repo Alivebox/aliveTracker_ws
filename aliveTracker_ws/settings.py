cbe396a482b9784b69c0d32e5e6a0a7a5408abda
pgonzalez <pgonzalez@alivebox.com>
2013-02-22 17:19:37 -0600

    add 'POST' to user_athentication

--- aliveTracker_ws/settings.py
+++ aliveTracker_ws/settings.py
@@ -117,7 +117,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'rest_framework',
+    'tastypie',
     'main',
 )
 
