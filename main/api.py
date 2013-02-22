eb52df1350842515e819a7a9f4082aa5262beb1c
Enrique Valverde <evalverde@alivebox.com>
2013-02-22 09:33:09 -0600

    Database created

--- main/api.py
+++ main/api.py
@@ -3,6 +3,7 @@ from tastypie.authorization import Authorization
 from tastypie.authentication import Authentication
 from tastypie.serializers import Serializer
 from main.models import Group
+from main.models import Project
 
 
 class GroupResource(ModelResource):
@@ -19,3 +20,14 @@ class GroupResource(ModelResource):
     #     return bundle
     # def determine_format(self, request):
         #     return 'application/json'
+
+
+class ProjectResource(ModelResource):
+    class Meta:
+        queryset = Project.objects.all()
+        resource_name = 'project'
+        authentication = Authentication()
+        authorization = Authorization()
+        excludes = ['entity_status']
+        allowed_methods = ['get', 'post', 'put', 'delete', 'patch']
+        serializer = Serializer(formats=['json'])
