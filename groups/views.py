38465e9621c8e04207151fcd6411226dfae10555
pgonzalez <pgonzalez@alivebox.com>
2013-02-26 16:00:07 -0600

    delete temporal ws

--- groups/views.py
+++ groups/views.py
@@ -1,5 +1,5 @@
-from main.models import Group_User
-from groups.serializers import Group_UserSerializer
+from main.models import Group_User, Role, Group
+from groups.serializers import Group_UserSerializer, GroupSerializer
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
@@ -18,17 +18,4 @@ def retrieveMyGroups(request,  format=None):
 
     if request.method == 'GET':
         serializer = Group_UserSerializer(tmpGroup)
-        return Response(serializer.data)
-
-
-@api_view(['GET','POST'])
-def retrieveGroupsIBelongTo(request, argPassword, argUserID, argUserName, format=None):
-
-    try:
-        tmpGroup = Group_User.objects.get(password=argPassword,user=argUserName,entity_status=0)
-    except Group_User.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
-        serializer = UserSerializer(tmpGroup)
         return Response(serializer.data)
