94e89e15b5bf2df420b44f715182c66ddb32022b
Andrs <andresd.garciag@gmail.com>
2013-02-27 14:15:59 -0600

    Project main with DTO method to get the permissions of a user in all of the groups he is in

--- main/urls.py
+++ main/urls.py
@@ -5,7 +5,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 admin.autodiscover(),
 urlpatterns = patterns('',
                        url(r'^admin/', include(admin.site.urls)),
-                       url(r'^main/$', 'main.views.user_authentication')
+                       url(r'^main/$', 'main.views.user_authentication'),
+                       url(r'^main/permissions/$', 'main.views.user_permissions')
                        )
 
 urlpatterns = format_suffix_patterns(urlpatterns)
