d38e974c35d225c05f35c3ce7b43c63574b5d560
pgonzalez <pgonzalez@alivebox.com>
2013-02-27 19:05:55 -0600

    Incomplete addLogEntry ws

--- groups/serializers.py
+++ groups/serializers.py
@@ -53,23 +53,23 @@ class Group_UserSerializer(serializers.ModelSerializer):
 class LogSerializer(serializers.ModelSerializer):
     class Meta:
         model = Log
-    fields = ('id', 'activity', 'time', 'date', 'user', 'project', 'group')
+    fields = ('id', 'activity', 'time', 'date', 'user', 'project', 'group', 'entity_status')
     pk = serializers.Field()
-    activity = serializers.Field
-    time = serializers.Field
-    date = serializers.CharField
+    activity = serializers.CharField()
+    time = serializers.IntegerField(default=0)
+    date = serializers.CharField()
     user = serializers.IntegerField()
     project = serializers.IntegerField()
     group = serializers.IntegerField()
-    entity_status = serializers.IntegerField()
+    entity_status = serializers.IntegerField(default=0)
 
     def restore_object(self, attrs, instance=None):
         if instance:
             instance.activity = attrs.get('activity', instance.activity)
             instance.time = attrs.get('time', instance.time)
             instance.date = attrs.get('date', instance.date)
-            instance.user = attrs.get('user', instance.user_id)
-            instance.project = attrs.get('project', instance.project_id)
+            instance.user = attrs.get('user', instance.user)
+            instance.project = attrs.get('project', instance.project)
             instance.group = attrs.get('group', instance.group)
             instance.entity_status = attrs.get('entity_status', instance.entity_status)
             return instance
