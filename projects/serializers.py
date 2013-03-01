ae5e0a4c8f537ecd8d2c9867c5fb96b788050bd9
Javier <jli@alivebox.com>
2013-03-01 16:23:16 -0600

    Implements deserializers.py. This will take the data from JSon and returns a model

--- projects/serializers.py
+++ projects/serializers.py
@@ -1,35 +1,17 @@
 from rest_framework import serializers
-from main.models import Project, User, Group
+from main.models import Project, Group
 from projects.dtos import ProjectUserDTO
-from main.utils import stringToDate
 
 
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
-    fields = ('id', 'name', 'description', 'created', 'entity_status', 'group')
-    pk = serializers.Field()  # Note: `Field` is an untyped read-only field.
-    name = serializers.CharField(required=True,max_length=30)
-    description = serializers.CharField(required=True,max_length=250)
-    created = serializers.CharField(required=True,max_length=250)
-    entity_status = serializers.IntegerField(default=0)
-    group = serializers.PrimaryKeyRelatedField()
-
-    def restore_object(self, attrs, instance=None):
-        """
-        Create or update a new snippet instance.
-        """
-        if instance:
-            # Update existing instance
-            instance.name = attrs.get('name', instance.name)
-            instance.description = attrs.get('description', instance.description)
-            instance.created = attrs.get('created', instance.created)
-            instance.entity_status = attrs.get('entity_status', instance.entity_status)
-            instance.group = (attrs.get('group', instance.group))
-            return instance
-
-        # Create new instance
-        return Project(**attrs)
+        fields = ('name', 'description', 'created', 'entity_status')
+        name = serializers.CharField(required=True,max_length=30)
+        description = serializers.CharField(required=True,max_length=250)
+        created = serializers.CharField(required=True,max_length=250)
+        entity_status = serializers.IntegerField(default=0)
+        group = serializers.PrimaryKeyRelatedField()
 
 
 class ProjectUserDTOSerializer(serializers.Serializer):
