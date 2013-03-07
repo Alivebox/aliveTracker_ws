d174beb51640e6baf47e38de237b5ce573b10af9
Enrique Valverde <evalverde@alivebox.com>
2013-03-07 08:55:50 -0600

    Logs Reports added to log.url and log.view

--- logs/urls.py
+++ logs/urls.py
@@ -3,6 +3,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = patterns('',
                        url(r'^logs(/group/(?P<group>([0-9]+)))?/$', 'logs.views.myLogsServices'),
+                       url(r'^logs/exportReport/$', 'logs.views.exportReport'),
                        )
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- logs/views.py
+++ logs/views.py
@@ -1,5 +1,6 @@
 from main.models import Log, Group
-from main.utils import userAuthentication, responseJsonUtil, getPropertyByName, getUserByRequest, groupExists, userIsGroupAdmin, userIsGroupMember, userIsProjectMember, projectExists
+from main.utils import *
+from logs.logReports import *
 from logs.serializers import LogSerializer
 from logs.deserializers import logDeserializer
 from django.db import transaction
@@ -45,6 +46,7 @@ def myLogsServices(request, group, format=None):
 def deleteLog(argUser, argGroup, argDate):
     Log.objects.filter(user=argUser, group=Group.objects.get(pk=argGroup), date=argDate).update(entity_status=1)
 
+
 def validateProject(request, argProjectID, argGroupID):
     if not groupExists(argGroupID):
         return 'ERROR200'
@@ -55,3 +57,45 @@ def validateProject(request, argProjectID, argGroupID):
     if not userIsProjectMember(request, argProjectID):
         return 'ERROR305'
     return None
+
+
+@api_view(['POST','GET'])
+def exportReport(request, format=None):
+    if request.method == 'GET':
+        reportBook = buildReport(3, 0, 0,0,'2012-01-01','2013-03-01')
+        return buildExcelFileResponse('logReport.xls', reportBook)
+    if userAuthentication(request):
+        if request.method == 'POST':
+            data = JSONParser().parse(request)
+            tmpGroupID = getPropertyByName('groupId',data.items())
+            tmpProjectID = getPropertyByName('projectId',data.items())
+            tmpUserID = getPropertyByName('userId',data.items())
+            tmpDateRangeId = getPropertyByName('dateRangeId',data.items())
+            tmpStartDate = getPropertyByName('startDate',data.items())
+            tmpEndDate = getPropertyByName('endDate',data.items())
+            errorCode = exportReportPermissionsValidation(tmpGroupID, tmpProjectID, tmpUserID)
+            if errorCode != None:
+                return responseJsonUtil(False, errorCode, None)
+            reportBook = buildReport(tmpGroupID, tmpProjectID, tmpUserID,tmpDateRangeId,tmpStartDate,tmpEndDate)
+            return buildExcelFileResponse('logReport.xls', reportBook)
+    else:
+        return responseJsonUtil(False, 'ERROR100', None)
+
+
+def exportReportPermissionsValidation(argGroupID,argProjectID,argUserID):
+    try:
+        Group.objects.get(id = argGroupID)
+    except:
+        return 'ERROR200'
+    if argProjectID!=0:
+        try:
+            Project.objects.get(id = argProjectID)
+        except:
+            return 'ERROR500'
+    if argUserID!=0:
+        try:
+            User.objects.get(id = argUserID)
+        except:
+            return 'ERROR400'
+    return None
+
