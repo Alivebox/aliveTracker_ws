5a3fcd2c9fcdcb7c4df11c50c903aa00b340f30b
Javier Li <jli@alivebox.com>
2013-03-18 16:04:09 -0600

    Add group ws tested and implemented

--- groups/urls.py
+++ groups/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import patterns, url
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = patterns('',
-                       url(r'^groups/(?P<pk>[0-9]+)$', 'groups.views.groupsServices'),
                        url(r'^groups/$', 'groups.views.groupsServices'),
+                       url(r'^groups/delete/(?P<argGroupID>[0-9]+)$', 'groups.views.deleteGroupProcess'),
                        url(r'^groups/getGroupsByUser/$', 'groups.views.getGroupsByUser'),
                        url(r'^users/group/(?P<argGroupID>[0-9]+)$', 'groups.views.getUsersByGroup'),
                        )
