380e264d2b34e49fbd2ff1732bba4d23c4107e13
Javier Li <jli@alivebox.com>
2013-03-21 11:22:13 -0600

    DB settings changed

--- aliveTracker_ws/settings.py
+++ aliveTracker_ws/settings.py
@@ -10,25 +10,24 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASES = {
-     # 'default': {
-     #    'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-     #    'NAME': 'alivetrackerdb',                      # Or path to database file if using sqlite3.
-     #    # The following settings are not used with sqlite3:
-     #    'USER': 'admin',
-     #    'PASSWORD': 'admin',
-     #    'HOST': '172.16.0.191',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-     #    'PORT': '5432',                      # Set to empty string for default.
-     # }
-
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'aliveTrackerdb',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',
-        }
+      'default': {
+         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+         'NAME': 'alivetrackerdb',                      # Or path to database file if using sqlite3.
+         # The following settings are not used with sqlite3:
+         'USER': 'admin',
+         'PASSWORD': 'admin',
+         'HOST': '172.16.0.191',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+         'PORT': '5432',                      # Set to empty string for default.
+      }
+    #'default': {
+     #   'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+      #  'NAME': 'aliveTrackerdb',                      # Or path to database file if using sqlite3.
+       # # The following settings are not used with sqlite3:
+        #'USER': '',
+        #'PASSWORD': '',
+        #'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        #'PORT': '',
+        #}
 }
 
 # Local time zone for this installation. Choices can be found here:
