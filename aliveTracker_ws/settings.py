57492b5d85349d7024134d5198db0d3aafc7d1b6
Javier Li <jli@alivebox.com>
2013-03-21 11:21:23 -0600

    DB settings changed

--- aliveTracker_ws/settings.py
+++ aliveTracker_ws/settings.py
@@ -19,6 +19,7 @@ DATABASES = {
      #    'HOST': '172.16.0.191',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
      #    'PORT': '5432',                      # Set to empty string for default.
      # }
+
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'aliveTrackerdb',                      # Or path to database file if using sqlite3.
