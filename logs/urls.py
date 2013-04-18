d9705b8bbcbb75002ac0b02619396e2d5c064994
julateAlivebox <julate@alivebox.com>
2013-04-18 10:01:18 -0600

    update user service

--- logs/urls.py
+++ logs/urls.py
@@ -3,7 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = patterns('',
                        url(r'^logs(/group/(?P<group>[0-9]+))?/$', 'logs.views.myLogsServices'),
-                       url(r'^logs/exportReport/$', 'logs.views.exportReport')
+                       url(r'^logs/exportReport/$', 'logs.views.exportReport'),
+                       url(r'^logs/listReport/group/(?P<group>[0-9]+)/project/(?P<project>[0-9]+)/user/(?P<user>[0-9]+)/range/(?P<range>[0-4]+)(/start/(?P<start>[A-Za-z0-9]+)/end/(?P<end>[A-Za-z0-9]+))?/$', 'logs.views.listReport'),
                        )
 
 urlpatterns = format_suffix_patterns(urlpatterns)
